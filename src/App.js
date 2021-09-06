fd52e90fd3efe0874dbf42be7586943b925158bd
fedor <fedor993@gmail.com>
2021-09-06 16:26:28 +0300

    Added state and method to change it

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import './App.css';
 import React from 'react';
 
 class App extends React.Component() {
+
+  state = {currentPage: 'home'};
+
+  navigateTo = (page) => {
+    this.setState({ currentPage: page })
+  } 
+
   render() {
     return <>App</>
   }
