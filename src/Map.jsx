0187908136cdaea7a5caf681e8a6373409d6bd45
fedor <fedor993@gmail.com>
2021-09-07 23:01:42 +0300

    Added access token for the mapbox

--- src/Map.jsx
+++ src/Map.jsx
@@ -4,6 +4,10 @@ import mapboxgl from "mapbox-gl";
 export class Map extends Component {
   mapContainer = React.createRef();
 
+  componentDidMount() {
+    mapboxgl.accessToken = 'pk.eyJ1IjoiZmVkb3I5OTMiLCJhIjoiY2t0YWh3c2gyMWY5NTJwcGhlb3Ixbmk2ZCJ9.0tOu7SLM1bKeXpkibT1CaQ';
+  }
+
   render() {
     return (
       <div className="map-wrapper">
