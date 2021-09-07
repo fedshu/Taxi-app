105961a330bb9df17af615c1776ad6dea5e7cf44
fedor <fedor993@gmail.com>
2021-09-07 23:06:43 +0300

    Added settings for the mapbox

--- src/Map.jsx
+++ src/Map.jsx
@@ -5,7 +5,15 @@ export class Map extends Component {
   mapContainer = React.createRef();
 
   componentDidMount() {
-    mapboxgl.accessToken = 'pk.eyJ1IjoiZmVkb3I5OTMiLCJhIjoiY2t0YWh3c2gyMWY5NTJwcGhlb3Ixbmk2ZCJ9.0tOu7SLM1bKeXpkibT1CaQ';
+    mapboxgl.accessToken =
+      "pk.eyJ1IjoiZmVkb3I5OTMiLCJhIjoiY2t0YWh3c2gyMWY5NTJwcGhlb3Ixbmk2ZCJ9.0tOu7SLM1bKeXpkibT1CaQ";
+
+    this.map = new mapboxgl.Map({
+        container: this.mapContainer.current,
+        style: 'mapbox://styles/mapbox/streets-v9',
+        center: [30.3056504, 59.9429126],
+        zoom: 10
+    })
   }
 
   render() {
