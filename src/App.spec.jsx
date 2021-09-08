ae398e886b03e0e6c6cb8737ef70e4b464acee18
fedor <fedor993@gmail.com>
2021-09-08 20:38:30 +0300

    Added Map component to test

--- src/App.spec.jsx
+++ src/App.spec.jsx
@@ -3,7 +3,7 @@ import App from './App'
 import {render, fireEvent} from '@testing-library/react'
 
 jest.mock('./Home', () => ({ Home: () => <div>Home component</div> }))
-jest.mock('./About', () => ({ About: () => <div>About component</div> }))
+jest.mock('./Map', () => ({ Map: () => <div>Map component</div> }))
 jest.mock('./Profile', () => ({ Profile: () => <div>Profile component</div> }))
 
 describe("App", () => {
@@ -12,12 +12,12 @@ describe("App", () => {
         expect(container.innerHTML).toMatch("Home component")
     })
 
-    describe("When clicking on navigation buttons", () => {
+    describe("When clicked on navigation buttons", () => {
         it("opens the corresponding page", () => {
             const { getByText, container } = render(<App />)
 
-            fireEvent.click(getByText('About'))
-            expect(container.innerHTML).toMatch('About component')
+            fireEvent.click(getByText('Map'))
+            expect(container.innerHTML).toMatch('Map component')
 
             fireEvent.click(getByText('Profile'))
             expect(container.innerHTML).toMatch('Profile component')
