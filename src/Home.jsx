12a461d5908f1feebba7b07a72605461186059e3
fedor <fedor993@gmail.com>
2021-09-09 13:19:32 +0300

    Implemented method to authenticate the user. Refactor

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,15 +1,31 @@
-import React from 'react'
-import { withAuth } from './AuthContext';
+import React from "react";
+import { withAuth } from "./AuthContext";
 
-export const Home = () => {
+export class Home extends React.Component {
+  authenticate = (event) => {
+    event.preventDefault();
+    const { email, password } = event.target;
+    this.props.logIn(email.value, password.value);
+  };
+
+  render() {
     return (
-        <form>
+      <>
+        {this.props.isLoggedIn ? (
+          <p>
+            You are logged in <button onClick={() => {}}>go to profile</button>
+          </p>
+        ) : (
+          <form onSubmit={this.authenticate}>
             <label htmlFor="email">Email:</label>
             <input id="email" type="email" name="email" size="28" />
             <label htmlFor="password">Password:</label>
             <input id="password" type="password" name="password" size="28" />
-        </form>
-    )
+          </form>
+        )}
+      </>
+    );
+  }
 }
 
-export const HomeWithAuth = withAuth(Home)
+export const HomeWithAuth = withAuth(Home);
