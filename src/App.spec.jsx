f4a882a6f25305ae1d9e05252ca6e6f9a48c647b
fedor <fedor993@gmail.com>
2021-09-10 19:44:51 +0300

    Implemented testing middleWare and routing

--- src/App.spec.jsx
+++ src/App.spec.jsx
@@ -1,26 +1,60 @@
-import React from 'react'
-import App from './App'
-import {render, fireEvent} from '@testing-library/react'
+import React from "react";
+import App from "./App";
+import { render, fireEvent } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { Router } from "react-router-dom";
+import { createMemoryHistory } from "history";
 
-jest.mock('./Home', () => ({ Home: () => <div>Home component</div> }))
-jest.mock('./Map', () => ({ Map: () => <div>Map component</div> }))
-jest.mock('./Profile', () => ({ Profile: () => <div>Profile component</div> }))
+jest.mock("./Home", () => ({ HomeWithAuth: () => <div>Home content</div> }));
+jest.mock("./Map", () => ({ Map: () => <div>Map content</div> }));
+jest.mock("./Profile", () => ({
+  ProfileWithAuth: () => <div>Profile content</div>,
+}));
 
 describe("App", () => {
-    it('renders correctly', () => {
-        const {container} = render(<App />)
-        expect(container.innerHTML).toMatch("Home component")
-    })
+  it("renders correctly", () => {
+    const mockStore = {
+      getState: () => ({ auth: { isLoggedIn: true } }),
+      subscribe: () => {},
+      dispatch: () => {},
+    };
 
-    describe("When clicked on navigation buttons", () => {
-        it("opens the corresponding page", () => {
-            const { getByText, container } = render(<App />)
+    const history = createMemoryHistory();
 
-            fireEvent.click(getByText('Map'))
-            expect(container.innerHTML).toMatch('Map component')
+    const { container } = render(
+      <Router history={history}>
+        <Provider store={mockStore}>
+          <App />
+        </Provider>
+      </Router>
+    );
 
-            fireEvent.click(getByText('Profile'))
-            expect(container.innerHTML).toMatch('Profile component')
-        })
-    })
-})
+    expect(container.innerHTML).toMatch("Home content");
+  });
+
+  describe("When clicked on navigation buttons", () => {
+    it("opens the corresponding page", () => {
+      const mockStore = {
+        getState: () => ({ auth: { isLoggedIn: true } }),
+        subscribe: () => {},
+        dispatch: () => {},
+      };
+  
+      const history = createMemoryHistory();
+  
+      const { container, getByText } = render(
+        <Router history={history}>
+          <Provider store={mockStore}>
+            <App />
+          </Provider>
+        </Router>
+      );
+  
+      fireEvent.click(getByText("Map"));
+      expect(container.innerHTML).toMatch("Map");
+      fireEvent.click(getByText("Profile"));
+      expect(container.innerHTML).toMatch("Profile");
+    });
+  });
+});
+
