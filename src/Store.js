8a9e7282f4e037e72f6c53fa13af832dd07147c2
fedor <fedor993@gmail.com>
2021-09-10 15:15:24 +0300

    Connected store with the middleware

--- src/Store.js
+++ src/Store.js
@@ -1,4 +1,5 @@
-import { createStore } from "redux";
-import rootReducer from './reducers';
+import { createStore, applyMiddleware } from "redux";
+import rootReducer from "./reducers";
+import { AuthMiddleware } from "./AuthMiddleware";
 
-export const store = createStore(rootReducer)
+export const store = createStore(rootReducer, applyMiddleware(AuthMiddleware));
