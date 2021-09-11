05f7d6cf4fecac56205d528b21ec7f025cfc515a
fedor <fedor993@gmail.com>
2021-09-11 15:04:53 +0300

    Rename component

--- src/Store.js
+++ src/Store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./reducers";
 import createSagaMiddleware from "@redux-saga/core";
-import { authSaga } from "./AuthSaga";
+import { authSaga } from "./authSaga";
 
 const sagaMiddleware = createSagaMiddleware();
 
