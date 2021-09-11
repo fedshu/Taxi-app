d74a8d8cf9b7ad1522bfc5e335ca63745017e532
fedor <fedor993@gmail.com>
2021-09-11 15:45:15 +0300

    Implemented test for auth saga

--- src/authSaga.spec.jsx
+++ src/authSaga.spec.jsx
@@ -1,21 +1,22 @@
-import { recordSaga } from "./recordSaga";
-import { authenticateSaga } from "./AuthSaga";
+import { authenticateSaga } from "./authSaga";
 import { authenticate } from "./actions";
+import { recordSaga } from "./recordSaga";
 
-jest.api("./api", () => ({ serverLogin: jest.fn(() => true) }));
+jest.mock("./api", () => ({ serverLogin: jest.fn(() => true) }));
 
-describe('authSaga', () => {
-    describe('authenticate', () => {
-        it('authenticates through api', async () => {
-            const dispatched = await recordSaga(
-                authenticateSaga,
-                authenticate('testlogin', 'testpassword')
-            )
-            expect(dispatched).toEqual([
-                {
-                    type: "LOG_IN"
-                }
-            ])
-        })
-    })
-})
+describe("authSaga", () => {
+  describe("authenticate", () => {
+    it("authenticates through api", async () => {
+      const dispatched = await recordSaga(
+        authenticateSaga,
+        authenticate("testlogin", "testpassword")
+      );
+      console.log("dispatchedawd222", dispatched);
+      expect(dispatched).toEqual([
+        {
+          type: "LOG_IN",
+        },
+      ]);
+    });
+  });
+});
