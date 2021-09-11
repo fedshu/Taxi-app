0a6391ef0bdb11e5114acd01b4791ae720e2e245
fedor <fedor993@gmail.com>
2021-09-11 17:33:13 +0300

    Moved function and HTML-markup to the LoginForm component

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,14 +1,8 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
-import { authenticate } from "../actions";
+import { LoginForm } from "./LoginForm";
 export class Home extends React.Component {
-  authenticate = (event) => {
-    event.preventDefault();
-    const { email, password } = event.target;
-    this.props.authenticate(email.value, password.value);
-  };
-
   render() {
     return (
       <>
@@ -17,20 +11,13 @@ export class Home extends React.Component {
             You are logged in <Link to="/profile">Go to profile</Link>
           </p>
         ) : (
-          <form onSubmit={this.authenticate}>
-            <label htmlFor="email">Email:</label>
-            <input id="email" type="email" name="email" size="28" />
-            <label htmlFor="password">Password:</label>
-            <input id="password" type="password" name="password" size="28" />
-            <button type="submit">Submit</button>
-          </form>
+          <LoginForm></LoginForm>
         )}
       </>
     );
   }
 }
 
-export const HomeWithAuth = connect(
-  (state) => ({isLoggedIn: state.auth.isLoggedIn}),
-  { authenticate }
-)(Home);
+export const HomeWithAuth = connect((state) => ({
+  isLoggedIn: state.auth.isLoggedIn,
+}))(Home);
