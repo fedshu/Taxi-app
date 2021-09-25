48a6c89a817fce188292cbf7172019e5fb4a1ce9
fedor <fedor993@gmail.com>
2021-09-25 22:13:33 +0300

    Imported component

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Map from "./Map";
 import { PrivateRoute } from "./PrivateRoute";
 import { ProfileWithAuth } from "./Profile";
+import { LoginForm } from "./LoginForm";
 
-export { Map, PrivateRoute, ProfileWithAuth };
+export { Map, PrivateRoute, ProfileWithAuth, LoginForm };
