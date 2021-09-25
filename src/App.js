43988f7a02387a06dc1352a17c25f37f2a881a35
fedor <fedor993@gmail.com>
2021-09-25 22:10:20 +0300

    Fix import path. Changed HTML-markup

--- src/App.js
+++ src/App.js
@@ -1,42 +1,42 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Switch, Route, Link } from "react-router-dom";
-import { PrivateRoute } from "./PrivateRoute";
+import { PrivateRoute, Map, ProfileWithAuth } from "./components";
 import { HomeWithAuth } from "./Home";
-import { Map } from "./Map";
-import { ProfileWithAuth } from "./Profile";
-import "./App.css";
-
+import { Paper } from "@mui/material";
 class App extends React.Component {
   render() {
     return (
       <>
-        {this.props.isLoggedIn && (
-          <header>
-            <nav>
-              <ul>
-                <li>
-                  <Link to="/">Home</Link>
-                </li>
-                <li>
-                  <Link to="/map">Map</Link>
-                </li>
-                <li>
-                  <Link to="/profile">Profile</Link>
-                </li>
-              </ul>
-            </nav>
-          </header>
-        )}
-        <main>
-          <section>
-            <Switch>
-              <Route exact path="/" component={HomeWithAuth} />
-              <PrivateRoute path="/map" component={Map} />
-              <PrivateRoute path="/profile" component={ProfileWithAuth} />
-            </Switch>
-          </section>
-        </main>
+        <Paper variant="outlined" square>
+          {this.props.isLoggedIn && (
+            <header>
+              <nav>
+                <ul>
+                  <li>
+                    <Link to="/">Home</Link>
+                  </li>
+                  <li>
+                    <Link to="/map">Map</Link>
+                  </li>
+                  <li>
+                    <Link to="/profile">Profile</Link>
+                  </li>
+                </ul>
+              </nav>
+            </header>
+          )}
+
+          <main>
+            <section>
+              <Switch>
+                <Route exact path="/" component={HomeWithAuth} />
+                <PrivateRoute path="/map" component={Map} />
+                <PrivateRoute path="/profile" component={ProfileWithAuth} />
+              </Switch>
+            </section>
+          </main>
+        </Paper>
       </>
     );
   }
