1650696ef7075d5179f239dd9a30c91519e79674
fedor <fedor993@gmail.com>
2021-09-26 20:00:26 +0300

    Changed form HTML-markup

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import { useDispatch } from "react-redux";
 import { authenticate } from "../actions";
-import { Paper, Button } from "@mui/material";
+import { Paper, Button, TextField, Grid, Box, Typography } from "@mui/material";
 
 export const LoginForm = ({ useDispatchHook = useDispatch }) => {
   const dispatch = useDispatchHook();
@@ -10,38 +10,49 @@ export const LoginForm = ({ useDispatchHook = useDispatch }) => {
 
   const onSubmit = (data) => {
     const { email, password } = data;
-    console.log("data", data);
     dispatch(authenticate(email, password));
   };
 
   return (
-    <Paper elevation={16}>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <label htmlFor="email">Email:</label>
-        <input
-          {...register("email", { required: true })}
-          id="email"
-          type="email"
-          name="email"
-          size="28"
-        />
-        <label htmlFor="password">Password:</label>
-        <input
-          {...register("password", { required: true })}
-          id="password"
-          type="password"
-          name="password"
-          size="28"
-        />
-        <Button type="submit" variant="contained" size="medium">
-          Log in
-        </Button>
-      </form>
-    </Paper>
+    <Grid container justifyContent="center" alignItems="center">
+      <Box>
+        <Paper elevation={16}>
+          <form className="form" onSubmit={handleSubmit(onSubmit)}>
+            <Typography variant="h5" gutterBottom component="div">
+              Log in
+            </Typography>
+            <Grid container direction="column">
+              <TextField
+                {...register("email", { required: true })}
+                id="email"
+                type="email"
+                name="email"
+                size="28"
+                label="Email"
+                variant="standard"
+                margin="normal"
+                className="form__field"
+              />
+              <TextField
+                {...register("password", { required: true })}
+                id="password"
+                type="password"
+                name="password"
+                size="28"
+                label="Password"
+                variant="standard"
+                margin="normal"
+                className="form__field"
+              />
+            </Grid>
+            <div className="form__button">
+              <Button type="submit" variant="outlined" size="medium">
+                Submit
+              </Button>
+            </div>
+          </form>
+        </Paper>
+      </Box>
+    </Grid>
   );
 };
-// authenticate = (event) => {
-//   event.preventDefault();
-//   const { email, password } = event.target;
-//   this.props.authenticate(email.value, password.value);
-// };
