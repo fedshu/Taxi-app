61aa2feb536533eda47e118a44bf4dd4f405c2ab
fedor <fedor993@gmail.com>
2021-09-27 21:04:15 +0300

    Changed HTML-markup. Refactor

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -9,22 +9,20 @@ export default class Map extends Component {
       "pk.eyJ1IjoiZmVkb3I5OTMiLCJhIjoiY2t0YWh3c2gyMWY5NTJwcGhlb3Ixbmk2ZCJ9.0tOu7SLM1bKeXpkibT1CaQ";
 
     this.map = new mapboxgl.Map({
-        container: this.mapContainer.current,
-        style: 'mapbox://styles/mapbox/streets-v9',
-        center: [30.3056504, 59.9429126],
-        zoom: 10
-    })
+      container: this.mapContainer.current,
+      style: "mapbox://styles/mapbox/streets-v9",
+      center: [30.3056504, 59.9429126],
+      zoom: 10,
+    });
   }
 
   componentWillUnmount() {
-      this.map.remove()
+    this.map.remove();
   }
 
   render() {
     return (
-      <div className="map-wrapper">
-        <div data-testid="map" className="map" ref={this.mapContainer}></div>
-      </div>
+      <div data-testid="map" className="map" ref={this.mapContainer}></div>
     );
   }
 }
